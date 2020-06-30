9837ce46e5e5bf249cfbdca1b54ef0dfb756831e
Jason <m.jason.liu@outlook.com>
2020-06-30 12:57:23 +0800

    high order function#1

--- main.hs
+++ main.hs
@@ -65,8 +65,45 @@ sort' (x:xs) =
 -- Tail recursion Fibonacci
 tailFib :: (Num a, Eq a, Enum a) => a -> a -> [a] -> a -> [a]
 tailFib _  _  result 0 = result
-tailFib cur next arr n = tailFib next (cur+next) (arr++[cur]) $ pred n}
+tailFib cur next arr n = tailFib next (cur+next) (arr++[cur]) $ pred n
 
--- High order function in Haskell >\=
+-- High order function in Haskell 
+sort'' :: (Ord a) => [a] -> [a]
+sort'' [] = []
+sort'' (x:xs) =
+    let smallerSort = sort'' $ filter (<x) xs
+        biggerSort = sort'' $ filter (>=x) xs
+    in smallerSort ++ [x] ++ biggerSort
 
-0
+-- Largest number less than 100,000 can divided by 3829
+largestDivisible :: (Integral a) => a
+largestDivisible = head $ filter p [100000, 99999..]
+    where p x = x `mod` 3829 == 0
+
+-- find the sum of all odd squares that are smaller than 10,000.
+oddSquaresLessThan10'000 :: (Integral a) => a
+oddSquaresLessThan10'000 = sum $ takeWhile (<10000) $ filter odd $ map (^2) $ [1..]
+
+oddSquaresLessThan10'000' :: (Integral a) => a
+oddSquaresLessThan10'000' = sum $ takeWhile (<10000) $ [oddSquare|x <- [1..], let oddSquare = x^2, (odd oddSquare)]
+
+{-
+For our next problem, we'll be dealing with Collatz sequences. 
+We take a natural number. If that number is even, we divide it by two. 
+If it's odd, we multiply it by 3 and then add 1 to that. 
+We take the resulting number and apply the same thing to it, 
+which produces a new number and so on. In essence, we get a chain of numbers.
+ It is thought that for all starting numbers, the chains finish at the number 1. 
+ So if we take the starting number 13, we get this sequence: 13, 40, 20, 10, 5, 16, 8, 4, 2, 1. 13*3 + 1 equals 40. 
+     40 divided by 2 is 20, etc. We see that the chain has 10 terms.
+-}
+
+-- for all starting numbers between 1 and 100, how many chains have a length greater than 15? 
+collatzSeq :: (Integral a, Num a) => a -> [a]
+collatzSeq 1        = [1]
+collatzSeq x
+    | (odd x)    = [x] ++ (collatzSeq $ x*3+1)
+    | (even x)   = [x] ++ (collatzSeq $ x `div` 2)
+    | otherwise = []
+--chainCount :: Int
+chainCount = length $ filter (>15) $ map length $ map collatzSeq [1..100]
